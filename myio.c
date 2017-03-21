14b2f91afaaf88e0fe29c70cdd1fa12c3217e732
IFree_X <1449089656@qq.com>
2017-03-21 16:10:23 +0800

    modify

--- myio.c
+++ myio.c
@@ -15,8 +15,10 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 
-#define PAGE_SIZE 8192     // 8KB
+#define PAGE_SIZE 4096     // 4KB
 #define SSD_BUFFER_SIZE 20*1024*1024*1024   // 20GB
+
+unsigned int write_magnification[] = {1,2,4,8,16,32,64,128};  // 4KB, 8KB, 16KB. 32KB, 64KB, 128KB, 256KB, 512KB
 
 
 char* buffer;
@@ -38,7 +40,7 @@ void init()
     }
 }
 
-void random_write(unsigned long num)
+void random_write(unsigned long num, int size)
 {
     unsigned long i, off_num;
     struct timeval tv_begin, tv_end;
@@ -47,7 +49,7 @@ void random_write(unsigned long num)
     int returnCode;
 
     gettimeofday(&tv_begin, NULL);
-    printf("-------------------begin to write---------------------\n");
+    printf("-------------------begin to write, write size = %dKB-------------------\n",size*4);
     for(i = 0; i < num; i++)
     {
         off_num = rand() % ((unsigned long)SSD_BUFFER_SIZE / (unsigned long)PAGE_SIZE / 20); // %(1GB/PAGE_SIZE)
@@ -62,7 +64,7 @@ void random_write(unsigned long num)
     totle_time = (tv_end.tv_usec - tv_begin.tv_usec) / 1000000.0 + (tv_end.tv_sec - tv_begin.tv_sec);
     bandWidth = (num * PAGE_SIZE) / (1024 * 1024) / totle_time;
     printf("totle time = %lf s, bandwidth = %lf MB\n", totle_time, bandWidth);
-    printf("------------------------end!------------------------\n");
+    printf("-------------------------------end!-----------------------------------\n\n");
 }
 
 int main()
@@ -75,7 +77,10 @@ int main()
         exit(1);
     }
     init();
-    random_write((unsigned long)SSD_BUFFER_SIZE / (unsigned long)PAGE_SIZE);
+    for(int i = 0; i < 8; i++)
+    {
+        random_write((unsigned long)SSD_BUFFER_SIZE / (unsigned long)(PAGE_SIZE*write_magnification[i]));
+    }
     close(fd);
     return 0;
 }
