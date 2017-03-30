58fe848a78b7dec4b1ac370b070922348f6a5ef9
xuchaoxikb <xuchaoxikb@gmail.com>
2017-03-30 20:09:54 +0800

    update add init

--- src/raid-5.c
+++ src/raid-5.c
@@ -6,5 +6,24 @@
  ************************************************************************/
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
 #include "raid-5.h"
+
+void initGlobalVirtualAddr()
+{
+    global_block_addr = (Global_Virtual_Addr*)malloc(sizeof(Global_Virtaul_Addr));
+    block_id = 0;
+    offset = BLKSIZE;
+    next = NULL;
+}
+
+void initRaidVirtualAddr()
+{
+    raid_virtual_addr = (Raid_Virtual_Addr*)malloc(sizeof(Raid_Virtual_Addr));
+    block_id = 0;
+    offset = BLKSIZE;
+    next = NULL;
+}
+
 
