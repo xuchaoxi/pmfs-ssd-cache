f54e14b4fea755aedb8b650ac0eb84cdf3880351
xuchaoxikb <xuchaoxikb@gmail.com>
2017-03-31 13:33:34 +0800

    update raid-5

--- src/raid-5.c
+++ src/raid-5.c
@@ -5,9 +5,13 @@
 	> Created Time: Thu 30 Mar 2017 04:52:18 PM CST
  ************************************************************************/
 
+#define _GNU_SOURCE 1
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/stat.h>
+#include<sys/file.h>
 #include "raid-5.h"
 
 void initGlobalVirtualAddr()
@@ -18,19 +22,44 @@ void initRaidVirtualAddr()
 {
 }
 
+void initBLKBuffer()
+{
+    int i;
+    block_buf = (char*)valloc(BLKSIZE);
+    if(!block_buf)
+    {
+        perror("[ERROR]:Fail to allocate blcok buffer");
+        exit(0);
+    }
+    for(i = 0;i < BLKSIZE;++i)
+        block_buf[i] = '.';
+}
+
 // param : id the Nth 4KB
-void execute(long id)
+void execute(long page_id)
 {   
-    long global_block_id = (long)( id / PAGENUM);  // the Nth block in the global virtual address
-    long global_stripe_id = (long)(global_block_id / 4); // the Nth stripe
-    int block_offset_stripe = global_block_id - global_stripe_id*4;  // the block offset in a stripe at global address
-    int parity_ssd_id = (global_stripe_id / 100) % 5; // the parity in Nth ssd
-    int block_ssd_id = (block_offset_stripe >= parity_ssd_id) ? block_offset_stripe+1 : block_offset_stripe;
-    printf("parity_ssd_id = %d\tblock_ssd_id = %d\tstripe_id = %d\n\n", parity_ssd_id, block_ssd_id, global_stripe_id);
+    global_block_id = (long)( page_id / PAGENUM);  // the Nth block in the global virtual address
+    global_stripe_id = (long)(global_block_id / 4); // the Nth stripe
+    block_offset_stripe = global_block_id - global_stripe_id*4;  // the block offset in a stripe at global address
+    parity_ssd_id = (global_stripe_id / rotate_width) % 5; // the parity in Nth ssd
+    data_ssd_id = (block_offset_stripe >= parity_ssd_id) ? block_offset_stripe+1 : block_offset_stripe;
+    printf("parity_ssd_id = %d\tblock_ssd_id = %d\tstripe_id = %d\n\n", parity_ssd_id, data_ssd_id, global_stripe_id);
     
 }
 
+void write_block()
+{
+    ssd_path[8] = data_ssd_id+'0';
+    int ssdfd = open(ssd_path, O_RDWR | O_DIRECT);
+    initBLKBuffer();
+    off_t offset = global_stripe_id;
+    int code = pwrite(ssdfd, block_buf, BLKSIZE, offset);   
+    if(code < 0)
+    {
+        perror("[ERROR]:Fail to write block buffer");
+        exit(0);
+    }
+    close(ssdfd);
+}
 
-
-    
 
--- src/raid-5.h
+++ src/raid-5.h
@@ -5,9 +5,6 @@
 	> Created Time: Thu 30 Mar 2017 04:52:52 PM CST
  ************************************************************************/
 
-#ifndef _RAID-5_H
-#define _RAID-5_H
-
 #define PAGESIZE 4096  // page size
 #define PAGENUM 128  // how many pages in a block
 
@@ -16,12 +13,20 @@ void initGlobalVirtualAddr();
 
 void initRaidVirtualAddr();
 
-void execute();
+void execute(long id);
+
 
 
 extern long GBLKNUM;  // Global block numbers
 extern long RBLKNUM;  // Raid block numbers
 extern long BLKSIZE;  // block size
-extern long rotate_width;  // rotate width for stripe
+extern long rotate_width;  // rotate width for stripeu
 
-#endif
+extern int data_ssd_id; // ssd id for data
+extern int parity_ssd_id; // ssd id for parity
+extern int block_offset_stripe; // the block offset in a stripe at global address
+extern long global_stripe_id; // the Nth stripe
+extern long global_block_id; // the Nth block in the global virtual address
+
+extern char *ssd_path; // ssd path
+extern char *block_buf; // block
