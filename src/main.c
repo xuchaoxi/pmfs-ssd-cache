c9cbb54df30c8954689b75df170df5b29f9e18c3
xuchaoxikb <xuchaoxikb@gmail.com>
2017-03-31 19:16:21 +0800

    update main

--- src/main.c
+++ src/main.c
@@ -8,18 +8,25 @@
 #define _GNU_SOURCE 1
 
 #include<stdio.h>
+#include<stdlib.h>
 #include"raid-5.h"
 
 int main()
 {
     int con, rw;
     long id;
+
+    struct timeval tv_begin, tv_end;
+    double total_time;
     
+    gettimeofday(&tv_begin, NULL);
     while(scanf("%d %d %ld", &con, &rw, &id)!=EOF)
     {
         execute(id);
-        writeBlock();
-        printf("%s\n", ssd_path);
+        writePage();
     }
+    gettimeofday(&tv_end, NULL);
+    total_time = (tv_end.tv_usec - tv_begin.tv_usec)/1000000.0+(tv_end.tv_sec - tv_begin.tv_sec);
+    printf("total time = %lf\n",total_time);
     return 0;
 }
