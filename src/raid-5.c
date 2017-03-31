cb642e40dee59527803fa2603963877a22865702
xuchaoxikb <xuchaoxikb@gmail.com>
2017-03-31 12:45:23 +0800

    update raid-5

--- src/raid-5.c
+++ src/raid-5.c
@@ -12,18 +12,25 @@
 
 void initGlobalVirtualAddr()
 {
-    global_block_addr = (Global_Virtual_Addr*)malloc(sizeof(Global_Virtaul_Addr));
-    block_id = 0;
-    offset = BLKSIZE;
-    next = NULL;
 }
 
 void initRaidVirtualAddr()
 {
-    raid_virtual_addr = (Raid_Virtual_Addr*)malloc(sizeof(Raid_Virtual_Addr));
-    block_id = 0;
-    offset = BLKSIZE;
-    next = NULL;
 }
 
+// param : id the Nth 4KB
+void execute(long id)
+{   
+    long global_block_id = (long)( id / PAGENUM);  // the Nth block in the global virtual address
+    long global_stripe_id = (long)(global_block_id / 4); // the Nth stripe
+    int block_offset_stripe = global_block_id - global_stripe_id*4;  // the block offset in a stripe at global address
+    int parity_ssd_id = (global_stripe_id / 100) % 5; // the parity in Nth ssd
+    int block_ssd_id = (block_offset_stripe >= parity_ssd_id) ? block_offset_stripe+1 : block_offset_stripe;
+    printf("parity_ssd_id = %d\tblock_ssd_id = %d\tstripe_id = %d\n\n", parity_ssd_id, block_ssd_id, global_stripe_id);
+    
+}
+
+
+
+    
 
--- src/raid-5.h
+++ src/raid-5.h
@@ -8,52 +8,20 @@
 #ifndef _RAID-5_H
 #define _RAID-5_H
 
+#define PAGESIZE 4096  // page size
+#define PAGENUM 128  // how many pages in a block
 
-typedef struct {
-    long block_id;   // block no
-    long stripe_id;   // block belong to strip
-    int ssd_id;         // block belong to ssd
-    int page_num;    // pages in a block, 128
-    int status;       //  block status
-    bool isparity;   // data block or parity block
-} Block;  // block
-
-typedef struct {
-    long stripe_id;   // stripe no
-    int block_num;   // blocks in a stripe // num = 5
-} Stripe;
-
-
-typedef struct {
-    int ssd_id;  // ssd no
-    long block_num;  //  how many blocks in a ssd
-} SSD;   // 5 SSDs  SSD[5] 
-
-typedef struct {
-    long block_id;  // mark block in global address
-    off_t offset;   // specify where the block
-    Global_Virtual_Addr *next;  // next block
-} Global_Virtual_Addr;
-
-typedef struct  {
-    long block_id;   // mark block in raid address
-    off_t offset;   // specify where the block 
-    Raid_Virtual_Addr *next;  // next block;
-} Raid_Virtual_Addr;
 
 void initGlobalVirtualAddr();
 
 void initRaidVirtualAddr();
 
+void execute();
 
 
 extern long GBLKNUM;  // Global block numbers
 extern long RBLKNUM;  // Raid block numbers
 extern long BLKSIZE;  // block size
-extern Global_Virtual_Addr *global_block_addr;
-extern Raid_Virtual_Addr *raid_virtual_addr;
-
-
-
+extern long rotate_width;  // rotate width for stripe
 
 #endif
