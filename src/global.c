1b42913017484102dec78003b3b3e4b45169bdd7
xuchaoxikb <xuchaoxikb@gmail.com>
2017-03-31 13:32:57 +0800

    update global.c

--- src/global.c
+++ src/global.c
@@ -10,6 +10,14 @@
 long GBLKNUM;  // global block numbers
 long RBLKNUM;  // raid block numbers
 long BLKSIZE = 128*4096;  // block size;
-long rotate_width; // rotate width for stripe
+long rotate_width = 100; // rotate width for stripe
 
+char *ssd_path = "/dev/sdb1"; 
+
+int data_ssd_id;
+int parity_ssd_id;
+int block_offset_stripe;
+long global_stripe_id;
+long global_block_id; 
+char *block_buf;
 
