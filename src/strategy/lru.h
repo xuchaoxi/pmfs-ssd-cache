86ed26a1eca143e11a22ee81596872f67306b730
xuchaoxikb <xuchaoxikb@gmail.com>
2017-04-04 20:36:21 +0800

    add lru.h

--- src/strategy/lru.h
+++ src/strategy/lru.h
@@ -8,6 +8,25 @@
 #ifndef _LRU_H
 #define _LRU_H
 
+#define DEBUG 0
+
+typedef struct 
+{
+    long nvm_buf_id; // nvm buffer location in shared buffer
+    long next_lru;  // to link used nvm as LRU
+    long last_lru;  
+} NVMBufferDescForLRU;
+
+typedef struct
+{
+    long first_lru; // Head of list of LRU
+    long last_lru;  // Tail of list of LRU
+} NVMBufferControlForLRU;
+
+NVMBufferDescForLRU *nvm_buffer_descriptor_lru;
+NVMBufferControlForLRU *nvm_buffer_control_lru;
+
+extern void initSSDBufferForLRU();
 extern void *hitInLRUBuffer(NVMBufferDesc *nvm_buf_hdr);
 extern NVMBufferDesc *getLRUBuffer();
 
